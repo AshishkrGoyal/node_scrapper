fb0d09f476b8e74d49a52bfa9872b2e94e511481
AshishkrGoyal <15103140ashish@gmail.com>
2017-08-06 02:03:01 +0530

    serverside_image_script_added

--- server_img/script.js
+++ server_img/script.js
@@ -1,9 +1,40 @@
 const Jimp = require('jimp');
+const fs = require('fs');
 
-Jimp.read('img/Ashish_PP.jpg',function (err,Ashish) {
-    if(err)
-    {
-        console.log(err);
-    }
-    Ashish.resize(200,200).sepia().write('out_img/Ashish_ref.png');
-});
+var arr=["Team_Profile/Aditya_Singh.png",
+    "Team_Profile/Ajay_Kushwaha.png",
+    "Team_profile/Akanksha_Maurya_2.png",
+    "Team_Profile/Ankita_Vaishali.png",
+    "Team_Profile/Anmol_.png",
+    "Team_Profile/Anuj_Saraswat_2.png",
+    "Team_Profile/Charu_Chauhan_2.png",
+    "Team_Profile/Devansh_Alok.png",
+    "Team_Profile/Dhristi_Mittal.png",
+    "Team_Profile/Hardik_17.png",
+    "Team_Profile/Kanishka_New.png",
+    "Team_Profile/Kanishka_Parmar.png",
+    "Team_Profile/Keya_Kriti.png",
+    "Team_Profile/Palash_Rai.png",
+    "Team_Profile/Parimal_2.png",
+    "Team_Profile/Priya_daBhardwaj.png",
+    "Team_Profile/Priyanshi_Gupta.png",
+    "Team_Profile/Skandha_Choudhary.png",
+    "Team_Profile/Yash_Goel.png",
+    "Team_Profile/Aditi_Saxena_1.png"
+]
+
+console.log(arr.length);
+
+for(let i=0; i<arr.length; i++)
+{
+    Jimp.read(arr[i],function (err,Aditi) {
+        if(err)
+        {
+            console.log(err);
+            throw err;
+        }
+        Aditi.resize(300,300).write("New_Images/"+arr[i]);
+    })
+}
+
+
